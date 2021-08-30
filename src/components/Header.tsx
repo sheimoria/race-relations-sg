13633691bb15fc96550ac44fc60d68998e52558d
Shem Maleriado <shem.maleriado@gmail.com>
2021-08-30 14:16:53 +0800

    feat: add dark mode

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,22 @@
+import { useTheme } from 'next-themes'
+import { MoonIcon, SunIcon } from '@heroicons/react/solid'
+
 const Header = () => {
+  const { theme, setTheme } = useTheme()
+
   return (
     <header className="p-6">
       <nav className="flex items-center justify-between gap-6">
-        <h1 className="font-semibold text-gray-700 dark:">
-          Race Relations in Singapore
-        </h1>
+        <h1>Race Relations in Singapore</h1>
+        <div className="flex items-center gap-4 transition">
+          {theme ? (
+            theme === 'light' ? (
+              <MoonIcon onClick={() => setTheme('dark')} />
+            ) : (
+              <SunIcon onClick={() => setTheme('light')} />
+            )
+          ) : null}
+        </div>
       </nav>
     </header>
   )
