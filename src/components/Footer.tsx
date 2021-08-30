c273c189495570c644fe6c92af74fb802de1c1a2
Shem Maleriado <shem.maleriado@gmail.com>
2021-08-30 15:05:12 +0800

    add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+  return (
+    <footer className="sticky bottom-0 flex items-center justify-between gap-4 p-6 bg-white dark:bg-gray-900">
+      <a
+        href="https://docs.google.com/spreadsheets/u/1/d/1VsoJpvVbRQjboAiCWU4gxO1TNzMTj0VFJ2I52BHlG1k/htmlview#"
+        className="footer-link"
+      >
+        Resource Bank by Adila
+      </a>
+      <a
+        href="https://www.instagram.com/sheimoria/?hl=en"
+        className="footer-link"
+      >
+        Website by Shem
+      </a>
+    </footer>
+  )
+}
+
+export default Footer
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import Footer from 'components/Footer'
 import Head from 'next/head'
 import Header from 'components/Header'
 import type { NextPage } from 'next'
@@ -17,7 +18,7 @@ const Home: NextPage = () => {
           This document is intended to serve as a resource bank for us to deepen
           our knowledge about race relations in Singapore. It was inspired by
           the compilation of{' '}
-          <a className="text-info" href="bit.ly/ANTIRACISMRESOURCES">
+          <a className="link" href="bit.ly/ANTIRACISMRESOURCES">
             Anti-Racism Resources document
           </a>{' '}
           compiled by Sarah Sophie Flicker and Alyssa Klein.
@@ -33,14 +34,11 @@ const Home: NextPage = () => {
           <br />
           In the course of my research, I have also come across useful scholarly
           articles and books that have been compiled and shared beforehand by{' '}
-          <a className="text-info" href="https://www.academia.sg/">
+          <a className="link" href="https://www.academia.sg/">
             Academia SG
           </a>{' '}
           and{' '}
-          <a
-            className="text-info"
-            href="https://twitter.com/ikansumbat?lang=en"
-          >
+          <a className="link" href="https://twitter.com/ikansumbat?lang=en">
             @ikansumbat
           </a>
           , and have included them in this list for ease of reference.
@@ -58,14 +56,14 @@ const Home: NextPage = () => {
           Special thanks to the following people for their contribution: Corrie
           Tan for the arts-related resources, Yogesh,{' '}
           <a
-            className="text-info"
+            className="link"
             href="https://www.instagram.com/pagesofelly/?hl=en"
           >
             @pagesofelly
           </a>
           ,{' '}
           <a
-            className="text-info"
+            className="link"
             href="https://www.instagram.com/nonfirqtion/?hl=en"
           >
             @nonfirqtion
@@ -79,7 +77,7 @@ const Home: NextPage = () => {
           <br />
           If you have any suggestions to add to the list or any feedback, please
           fill in the form{' '}
-          <a className="text-info" href="https://forms.gle/stgnm6FdhrZpMoi17">
+          <a className="link" href="https://forms.gle/stgnm6FdhrZpMoi17">
             here
           </a>
           .
@@ -88,14 +86,12 @@ const Home: NextPage = () => {
           Thank you!
           <br />
           <br />
-          <a
-            className="text-info"
-            href="https://www.instagram.com/adila_s/?hl=en"
-          >
+          <a className="link" href="https://www.instagram.com/adila_s/?hl=en">
             Adila
           </a>
         </p>
       </main>
+      <Footer />
     </>
   )
 }
