2fa970085f96529f5057a155849a74a3b4d123b9
Shem Maleriado <shem.maleriado@gmail.com>
2021-08-30 14:01:00 +0800

    feat: add next themes

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header className="p-6">
+      <nav className="flex items-center justify-between gap-6">
+        <h1 className="font-semibold text-gray-700 dark:">
+          Race Relations in Singapore
+        </h1>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
+import Header from 'components/Header'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  return <div>Hi</div>
+  return <Header />
 }
 
 export default Home
