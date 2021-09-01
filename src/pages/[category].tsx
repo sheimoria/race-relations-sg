818da95c1284af6dcdb9c330a010283e2f8dc5f5
Shem Maleriado <shem.maleriado@gmail.com>
2021-09-01 11:58:59 +0800

    fix: credentials

--- src/pages/[category].tsx
+++ src/pages/[category].tsx
@@ -40,6 +40,21 @@ export const getServerSideProps = async ({
   query: { category: string }
 }) => {
   const auth = await google.auth.getClient({
+    credentials: {
+      type: 'service_account',
+      //@ts-ignore
+      project_id: 'plenary-utility-279217',
+      private_key_id: process.env.PRIVATE_KEY_ID,
+      private_key: process.env.PRIVATE_KEY,
+      client_email:
+        'race-relations-in-singapore@plenary-utility-279217.iam.gserviceaccount.com',
+      client_id: process.env.CLIENT_ID,
+      auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_uri: 'https://oauth2.googleapis.com/token',
+      auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+      client_x509_cert_url:
+        'https://www.googleapis.com/robot/v1/metadata/x509/race-relations-in-singapore%40plenary-utility-279217.iam.gserviceaccount.com'
+    },
     scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly']
   })
 
